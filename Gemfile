295f4ea5f667144d0a0d7e68465f131e5552af54
Mohammed Sadiq <sadiqmmm@gmail.com>
2015-10-23 20:24:13 +0530

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in smartcrop-rails.gemspec
+gemspec
---
+++ lib/smartcrop/rails.rb
@@ -0,0 +1,7 @@
+require "smartcrop/rails/version"
+
+module Smartcrop
+  module Rails
+    # Your code goes here...
+  end
+end
